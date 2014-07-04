175671a209b084a8102cb98aa2db75ef5117c8fa
Christopher Su <christophersu9@gmail.com>
2014-07-03 21:45:24 -0700

    working mark as completed from done_tasks

--- tasksync/TaskSync.py
+++ tasksync/TaskSync.py
@@ -6,7 +6,7 @@ class TaskSync():
         for tasklist in tasklists['items']:
             print json.dumps(tasklist)
 
-    def sync(self, tasklist):
+    def sync(self, google_tasklist, tasks_to_sync=None, done_tasks=None):
         # eventually: sync(self, json, tasklist)
         # assume you receive data in the following format
         tasks_to_sync = [
@@ -17,21 +17,30 @@ class TaskSync():
             }
         ]
 
+        # get these by either looking at the last 100 events or getting all tasks from done
+        done_tasks = [
+            "41911c31f5629ad3c6ec7aa504000c40"
+        ]
+
         # get tasks that are already there
-        existing = self.service.tasks().list(tasklist=tasklist).execute()
+        existing = self.service.tasks().list(tasklist=google_tasklist).execute()
 
         # check for duplicates
         for task in existing['items']:
             for i in range(0, len(tasks_to_sync)):
+                cur_id = tasks_to_sync[i]['_id']
                 if 'notes' in task.keys():
-                    if task['notes'] == tasks_to_sync[i]['_id']:
-                        print 'found duplicate'
-                        # check to see if already imported tasks have changed in state
+                    if task['notes'] == cur_id :
+                        # print 'found duplicate'
+                        if cur_id in done_tasks:
+                            task['status'] = 'completed'
+                            result = self.service.tasks().update(tasklist=google_tasklist, task=task['id'], body=task).execute()
+                            # print result['completed']
                         tasks_to_sync.pop(i)
 
         # add new tasks
         for task in tasks_to_sync:
-            result = self.service.tasks().insert(tasklist=tasklist, body={
+            result = self.service.tasks().insert(tasklist=google_tasklist, body={
                 'title': task['name'],
                 'notes': task['_id'],
                 'due': task['due']
